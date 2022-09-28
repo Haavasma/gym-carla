209a886b2555f9d8ff2d44200b1c0335ea817b13
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2022-09-28 21:18:59 +0200

    added specification of module

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_carla',
       version='0.0.1',
-      install_requires=['gym', 'pygame'] 
+      install_requires=['gym', 'pygame'],
+      py_modules=['gym_carla'],
 )
